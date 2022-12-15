a1d0c553426ea564413ca991472f56a8a08477bf
Christian Barnes <70176952+ChristianKBarnes@users.noreply.github.com>
2022-12-15 18:35:27 +0000

    Added tailwind config and css

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+module.exports = {
+	darkMode: 'class',
+	theme: {
+		fontFamily: {
+			sans: ['Plus Jakarta Sans', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				primary: 'var(--color-primary)',
+				secondary: 'rgba(var(--color-secondary), <alpha-value>)',
+				danger: 'var(--color-dangger)',
+				transparent: 'transparent',
+				current: 'currentColor',
+				blue: '#1fb6ff',
+				purple: '#635FC7',
+				'purple-light': '#A8A4FF',
+				'gray-dark': '#20212C',
+				gray: '#828FA3',
+				'gray-light': '#F4F7FD',
+				black: '#000112',
+				'black-light': '#2B2C37',
+				light: '#E4EBFA',
+				dark: '#3E3F4E',
+				white: '#FFFFFF',
+				red: '#EA5555',
+				'red-light': '#FF9898',
+			},},
+	},
+}
