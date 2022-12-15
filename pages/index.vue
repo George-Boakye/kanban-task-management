180744408594ee4891ae4fdb06c7671d347cec33
Christian Barnes <70176952+ChristianKBarnes@users.noreply.github.com>
2022-12-15 18:45:26 +0000

    Updated index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,9 @@
 <template>
-  <h1>hhh</h1>
+  <div>
+    <h2>Dashboard</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis omnis consequatur quod necessitatibus autem repellat aperiam quia atque fugit praesentium earum rerum minus, porro asperiores explicabo inventore animi ducimus. Itaque voluptatum facilis nobis, praesentium, quasi pariatur veniam veritatis quo eligendi, ratione libero impedit. Distinctio, delectus sapiente adipisci assumenda obcaecati aliquid exercitationem sit ad autem! Veritatis nihil accusamus recusandae esse?</p>
+  </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
+<style scoped>
 </style>
