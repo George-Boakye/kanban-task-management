58feeb038b82bde6f3ab02c0d2b1e20d95fb1328
Christian Barnes <70176952+ChristianKBarnes@users.noreply.github.com>
2022-12-15 18:37:10 +0000

    Added Navbar.vue component

---
+++ components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+	<header class="relative flex items-center justify-start bg-white">
+		<nav class="flex flex-nowrap w-full items-center justify-between">
+			<a class="ml-10 mr-9 pr-8 p-0 border border-y-0 border-l-0 border-r-light py-10" href="#">
+				<img src="~/assets/images/logo-dark.svg" class="h-6 mr-3" alt="Flowbite Logo" />
+			</a>
+			<div class="flex items-center flex-grow">
+				<span class="heading-xl">Platform Launch</span>
+				<button class="btn btn-primary ml-auto">
+					<span class="mr-2"
+						><img src="~/assets/images/icon-add-task-mobile.svg" alt="X"
+					/></span>
+					Add New Task
+				</button>
+				<span class="mx-8"
+					><img src="~/assets/images/icon-vertical-ellipsis.svg" alt="vertical" />
+				</span>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
