bf254892992211a54bbd04eb1990e41c650e1b98
Christian Barnes <70176952+ChristianKBarnes@users.noreply.github.com>
2022-12-15 18:36:19 +0000

    Updated app configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	modules: ['@nuxtjs/tailwindcss'],
+	app: {
+		head: {
+			title: 'Kanban Task Management',
+			link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		},
+	},
 })
