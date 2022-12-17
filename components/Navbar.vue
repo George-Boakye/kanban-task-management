e5fa67c734b86d707c79244579d225bb290686ee
Christian Barnes <70176952+ChristianKBarnes@users.noreply.github.com>
2022-12-17 20:48:52 +0000

    Updated Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,18 @@
 <template>
 	<header class="relative flex items-center justify-start bg-white">
 		<nav class="flex flex-nowrap w-full items-center justify-between">
-			<a class="ml-10 mr-9 pr-8 p-0 border border-y-0 border-l-0 border-r-light py-10" href="#">
+			<a
+				class="ml-10 mr-9 pr-8 p-0 border border-y-0 border-l-0 border-r-light py-10"
+				href="#">
 				<img src="~/assets/images/logo-dark.svg" class="h-6 mr-3" alt="Flowbite Logo" />
 			</a>
 			<div class="flex items-center flex-grow">
-				<span class="heading-xl">Platform Launch</span>
+				<span class="heading-xl" :class="{ 'ml-[81px]': showSidebar }"
+					>Platform Launch</span
+				>
 				<button class="btn btn-primary ml-auto">
-					<span class="mr-2"
-						><img src="~/assets/images/icon-add-task-mobile.svg" alt="X"
-					/></span>
-					Add New Task
+					<SvgComponent name="icon-add-task-mobile" />
+					<span class="hidden md:block">Add New Task</span>
 				</button>
 				<span class="mx-8"
 					><img src="~/assets/images/icon-vertical-ellipsis.svg" alt="vertical" />
@@ -20,6 +22,10 @@
 	</header>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+defineProps({
+	showSidebar: Boolean,
+})
+</script>
 
 <style scoped></style>
