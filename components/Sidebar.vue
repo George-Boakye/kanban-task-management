204d27d907aba23e868940f211dc65f65ffefaa4
Christian Barnes <70176952+ChristianKBarnes@users.noreply.github.com>
2022-12-18 08:35:32 +0000

    Updated Sidebar.vue

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -2,10 +2,10 @@
 	<div>
 		<aside v-show="showSidebar" class="sidebar" aria-label="Sidebar">
 			<a class="py-10 px-8 align-middle" href="#">
-				<img src="~/assets/images/logo-dark.svg" class="h-6" alt="Flowbite Logo" />
+				<SvgComponent name="logo" class="h-6 text-black dark:text-white" />
 			</a>
 
-			<div class="py-3">
+			<div class="py-3 overflow-auto">
 				<h5 class="heading-sm mb-3 uppercase px-8">ALL BOARDS (3)</h5>
 				<ul class="sidebar-list">
 					<li class="sidebar-item">
@@ -27,12 +27,6 @@
 						>
 					</li>
 					<li class="sidebar-item">
-						<NuxtLink to="#" class="sidebar-link">
-							<SvgComponent name="icon-board" />
-							Products</NuxtLink
-						>
-					</li>
-					<li class="sidebar-item">
 						<NuxtLink href="#" class="sidebar-link text-purple items-center">
 							<SvgComponent name="icon-add-task-mobile" class="text-currentColor" />
 							Create New Board
@@ -42,13 +36,13 @@
 			</div>
 			<div class="px-3.5 relative mt-auto mb-6">
 				<div
-					class="bg-gray-light flex justify-center items-center py-3 rounded-md svg-mr-0">
+					class="bg-gray-light flex justify-center items-center py-3 rounded-md svg-mr-0 dark:bg-black-dark">
 					<SvgComponent name="icon-light-theme" />
 
 					<label class="theme-toggle">
-						<input type="checkbox" value="" class="sr-only peer" />
+						<input type="checkbox" value="" class="sr-only peer" @click="toggleTheme" />
 						<div
-							class="w-11 h-6 bg-purple rounded-full peer peer-focus:ring-2 peer-focus:ring-purple-light peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+							class="w-11 h-6 bg-purple rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
 					</label>
 
 					<SvgComponent name="icon-dark-theme" />
@@ -78,4 +72,18 @@
 defineProps({
 	showSidebar: Boolean,
 })
+
+const theme = ref('light')
+
+const toggleTheme = () => {
+	console.log('changing theme')
+
+	if (theme.value !== 'dark') {
+		theme.value = 'dark'
+		return document.documentElement.classList.add('dark')
+	}
+
+	theme.value = 'light'
+	return document.documentElement.classList.remove('dark')
+}
 </script>
