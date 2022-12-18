694171e41b0db29f1e369ab8bb6c62e7b87053db
Christian Barnes <70176952+ChristianKBarnes@users.noreply.github.com>
2022-12-18 08:34:45 +0000

    Upadated Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-	<header class="relative flex items-center justify-start bg-white border-b border-b-light">
+	<header class="relative flex items-center justify-start bg-white border-b border-b-light text-black dark:bg-gray-dark dark:border-dark dark:text-white">
 		<nav class="flex flex-nowrap w-full items-center justify-between">
 			<a
-				class="ml-10 mr-9 pr-8 p-0 border border-y-0 border-l-0 border-r-light py-10"
+				class="ml-10 mr-9 pr-8 p-0 border border-y-0 border-l-0 border-r-light py-10 dark:border-r-dark"
 				href="#">
-				<img src="~/assets/images/logo-dark.svg" class="h-6 mr-3" alt="Flowbite Logo" />
+				<SvgComponent name="logo" class="h-6 mr-3 text-black dark:text-white" />
 			</a>
 			<div class="flex items-center flex-grow">
 				<span class="heading-xl" :class="{ 'ml-[81px]': showSidebar }"
