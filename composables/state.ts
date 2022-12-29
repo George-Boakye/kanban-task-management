e3ef9bca7ae129068797d18b3320bc03dc7bd9ab
Qhristian <christian.kojobarnes@gmail.com>
2022-12-29 20:08:48 +0000

    Update state.ts

--- composables/state.ts
+++ composables/state.ts
@@ -1,3 +1,4 @@
 import data from '../data.json'
 
 export const useBoard = () => useState<[object]>('boards', () => data.boards)
+export const useSidebar = () => useState<boolean>('sidebar', () => true)
